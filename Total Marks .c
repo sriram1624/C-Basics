c43bbb74766a7d8d46692d7917c31c659d44f203
sriram1624 <92290567+sriram1624@users.noreply.github.com>
2021-10-25 12:19:44 +0530

    Add files via upload

---
+++ Total Marks .c
@@ -0,0 +1,19 @@
+#include<stdio.h>	
+void main()
+{
+	  // Vairaibles 
+	  int phy,che,mat,eng,tot;
+	  // Initialization
+	  phy = 50;
+	  che = 60;
+	  mat = 70;
+	  eng = 80;
+	  // Calculation
+	  tot = phy+che+mat+eng;
+	  // Output
+	  printf("marks in physics: %d\n",phy);
+	  printf("mqrks in chemistry: %d\n",che);
+	  printf("marks in maths: %d\n",mat);
+	  printf("marks in english: %d\n",eng);
+	  printf("total marks: %d\n",tot);
+}
